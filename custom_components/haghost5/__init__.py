e15920903a1f5895d31309604c3694fca7686c81
Mauro Morello <famiglia.morello@gmail.com>
2024-12-22 00:09:37 +0100

    Update __init__.py

--- custom_components/haghost5/__init__.py
+++ custom_components/haghost5/__init__.py
@@ -1,1 +1,16 @@
+from .const import DOMAIN
 
+async def async_setup(hass, config):
+    """Set up the HAGhost5 component."""
+    hass.data[DOMAIN] = {}
+    return True
+
+async def async_setup_entry(hass, config_entry):
+    """Set up HAGhost5 from a config entry."""
+    hass.data[DOMAIN][config_entry.entry_id] = config_entry.data
+    return True
+
+async def async_unload_entry(hass, config_entry):
+    """Unload a config entry."""
+    hass.data[DOMAIN].pop(config_entry.entry_id)
+    return True
