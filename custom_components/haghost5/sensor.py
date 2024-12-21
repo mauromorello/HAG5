320a30d3e6b26286aa2c5dea3eb75362c2b10e7f
Mauro Morello <famiglia.morello@gmail.com>
2024-12-22 00:12:13 +0100

    Create sensor.py

---
+++ custom_components/haghost5/sensor.py
@@ -0,0 +1,30 @@
+
+from .const import DOMAIN
+
+async def async_setup_entry(hass, config_entry, async_add_entities):
+    """Set up HAGhost5 sensor based on a config entry."""
+    ip_address = config_entry.data["ip_address"]
+    async_add_entities([HAGhost5Sensor(ip_address)])
+
+class HAGhost5Sensor(Entity):
+    def __init__(self, ip_address):
+        self._ip_address = ip_address
+        self._state = None
+
+    @property
+    def name(self):
+        return "HAGhost5 Sensor"
+
+    @property
+    def state(self):
+        return self._state
+
+    async def async_update(self):
+        """Fetch new state data for the sensor."""
+        # Example of connecting to the WebSocket server at the configured IP
+        from aiohttp import ClientSession, WSMsgType
+        async with ClientSession() as session:
+            async with session.ws_connect(f"ws://{self._ip_address}:port") as ws:
+                msg = await ws.receive()
+                if msg.type == WSMsgType.TEXT:
+                    self._state = msg.data
