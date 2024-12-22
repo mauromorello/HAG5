9ebd7cc2062a91bb02758e2ef785f0b20007bfbe
Mauro Morello <famiglia.morello@gmail.com>
2024-12-22 10:49:02 +0100

    Update sensor.py

--- custom_components/haghost5/sensor.py
+++ custom_components/haghost5/sensor.py
@@ -7,7 +7,9 @@ _LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
     """Set up the WebSocket sensor."""
-    ip_address = config_entry.data["ip_address"]
+    _LOGGER.debug("async_setup_entry called for HAGhost5")
+    ip_address = config_entry.data.get("ip_address")
+    _LOGGER.debug("IP address from config: %s", ip_address)
     async_add_entities([WebSocketSensor(ip_address)], True)
 
 class WebSocketSensor(Entity):
@@ -15,6 +17,7 @@ class WebSocketSensor(Entity):
 
     def __init__(self, ip_address):
         """Initialize the sensor."""
+        _LOGGER.debug("Initializing WebSocket sensor with IP: %s", ip_address)
         self._ip_address = ip_address
         self._state = None
 
@@ -31,14 +34,14 @@ class WebSocketSensor(Entity):
     async def async_update(self):
         """Fetch new state data for the sensor."""
         ws_url = f"ws://{self._ip_address}:8081/"
+        _LOGGER.debug("Connecting to WebSocket at: %s", ws_url)
         try:
             async with ClientSession() as session:
                 async with session.ws_connect(ws_url) as ws:
-                    _LOGGER.info("Connected to WebSocket at %s", ws_url)
                     msg = await ws.receive()
                     if msg.type == WSMsgType.TEXT:
                         self._state = msg.data
-                        _LOGGER.info("Received data: %s", msg.data)
+                        _LOGGER.debug("Received data: %s", msg.data)
                     elif msg.type == WSMsgType.ERROR:
                         _LOGGER.error("WebSocket error: %s", msg)
         except Exception as e:
