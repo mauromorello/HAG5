10788912a5775cb1b6693fc103840d9a10cc9597
Mauro Morello <famiglia.morello@gmail.com>
2024-12-23 14:32:43 +0100

    Update __init__.py

--- custom_components/haghost5/__init__.py
+++ custom_components/haghost5/__init__.py
@@ -30,6 +30,8 @@ async def async_setup_entry(hass, config_entry):
     hass.async_create_task(
         hass.config_entries.async_forward_entry_setup(config_entry, "sensor")
     )
+    
+    hass.config_entries.async_setup_platforms(entry, ["sensor"])
 
 
     return True
