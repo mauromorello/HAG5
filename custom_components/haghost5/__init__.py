9d6500f68fcab756e23adb815741ddd6f998b57a
Mauro Morello <famiglia.morello@gmail.com>
2024-12-24 13:41:07 +0100

    Update __init__.py

--- custom_components/haghost5/__init__.py
+++ custom_components/haghost5/__init__.py
@@ -88,8 +88,7 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry):
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Unload the integration."""
     await hass.config_entries.async_forward_entry_unload(entry, "sensor")
-    hass.http.unregister_view(GCodeUploadView)
-    hass.http.unregister_view(GCodeUploadAndPrintView)
+
     return True
 
 
